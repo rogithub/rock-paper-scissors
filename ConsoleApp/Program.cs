f41fc0bd7458d6d7a2394a492d4190ca4656d57d
rodrigo debian <correo.rodrigo@gmail.com>
2021-02-06 14:27:33 -0600

    Readme

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,9 @@ namespace ConsoleApp
 {
     class Program
     {
+        static HigherWinRateStrategy _strategy = new HigherWinRateStrategy();
+        static Game _game = new Game(_strategy);
+
         private static bool Validate(char strNumber, out int iMove)
         {
             iMove = -1;
@@ -24,16 +27,17 @@ namespace ConsoleApp
         private static void PrintResult(Round r)
         {
             string winner = r.IsTie ? "Tie" : r.UserWins ? "User Wins" : "Computer Wins";
+            string op = r.IsTie ? "==" : r.UserWins ? ">" : "<";
+
             if (r.UserWins) Console.ForegroundColor = ConsoleColor.Green;
             if (!r.UserWins && !r.IsTie) Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"[{winner}], User: {r.UserMove}, Computer: {r.ComputerMove}");
+
+            Console.WriteLine($"{_game.Rounds.IndexOf(r)} | {winner.PadRight(13, ' ')} | User: {r.UserMove} {op} Computer: {r.ComputerMove}");
             Console.ResetColor();
         }
+
         static void Main(string[] args)
         {
-            var strategy = new HigherWinRateStrategy();
-            var game = new Game(strategy);
-
             char strMove = '0';
             while (strMove != 'q')
             {
@@ -49,23 +53,35 @@ namespace ConsoleApp
                 }
 
                 Move userMove = (Move)iMove;
-                var r = game.AddUserMove(userMove);
+                var r = _game.AddUserMove(userMove);
                 PrintResult(r);
                 Console.ReadKey();
             }
+            Console.Clear();
 
             Func<int, int, double> percentage = (total, n) => total == 0 ? 0 : (n * 100) / total;
-            Console.WriteLine("Results \t Times \t Percent");
-            Console.WriteLine("===============================");
-            var arr = game.Rounds.ToArray();
+
+            Console.WriteLine("================================================");
+            Console.WriteLine("Results");
+            Console.WriteLine("================================================");
+            var arr = _game.Rounds.ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                PrintResult(arr[i]);
+            }
+
+
+            Console.WriteLine("================================================");
+
             var userWins = arr.Count(round => round.UserWins);
             var ties = arr.Count(round => round.IsTie);
             var computerWins = arr.Count(round => !round.IsTie && !round.UserWins);
 
-            Console.WriteLine($"Total Rounds    \t{arr.Length}");
-            Console.WriteLine($"User Wins       \t{userWins} \t{percentage(arr.Length, userWins)}%");
-            Console.WriteLine($"Computer Wins   \t{computerWins} \t{percentage(arr.Length, computerWins)}%");
-            Console.WriteLine($"Ties            \t{ties} \t{percentage(arr.Length, ties)}%");
+            Console.WriteLine($"Total Rounds    {arr.Length}");
+            Console.WriteLine($"User Wins       {userWins} ({percentage(arr.Length, userWins)}%)");
+            Console.WriteLine($"Computer Wins   {computerWins} ({percentage(arr.Length, computerWins)}%)");
+            Console.WriteLine($"Ties            {ties} ({percentage(arr.Length, ties)}%)");
+            Console.WriteLine("================================================");
         }
     }
 }
